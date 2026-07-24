f3d0e06ceb5d12f4d9d3178d10f437b84e22fb41
Shivam Bhardwaj <shiva.bhardwaj.mks@gmail.com>
2026-07-25 00:04:22 +0530

    Optimised Portfolio

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -25,26 +24,3 @@ export default tseslint.config(
     },
   },
 );
-=======
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
-
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
-    },
-  },
-])
->>>>>>> ad1b85d8693f1fc27a366246afa2cffa8df96f80
